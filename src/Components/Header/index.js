040d2d7c004e2bce2a5f24fd24ee1633621c3be1
MARIANO-89 <nano_carp@hotmail.com>
2021-05-01 21:52:36 -0300

    styles

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import { setMovies } from '../../actions/movies';
 import { useDispatch } from 'react-redux';
+import './index.css';
 function Header() {
     const [value, setValue] = useState('');
     const dispatch = useDispatch();
     function searching() {
-        fetch('https://api.themoviedb.org/3/search/movie?api_key=7559ebc59bb34de6c16531a9fa0fa55e&query=' + value).then(
-            function (response) {
+        if (value.length > 0) {
+            fetch(
+                'https://api.themoviedb.org/3/search/movie?api_key=7559ebc59bb34de6c16531a9fa0fa55e&query=' + value
+            ).then(function (response) {
                 response.json().then(function (data) {
                     dispatch(setMovies(data.results));
                 });
-            }
-        );
+            });
+        }
     }
     function getTopRated() {
         fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=7559ebc59bb34de6c16531a9fa0fa55e').then(function (
@@ -23,18 +26,37 @@ function Header() {
         });
     }
     return (
-        <header>
-            <input
-                name='search'
-                type='search'
-                value={value}
-                onChange={function (event) {
-                    setValue(event.target.value);
-                }}
-            />
+        <header className='header bg-dark'>
+            <div className='search'>
+                <div className='input-group input-group-lg'>
+                    <div className='input-group-prepend'>
+                        <span className='input-group-text' id='inputGroup-sizing-lg'>
+                            🔎
+                        </span>
+                    </div>
 
-            <button onClick={searching}>search</button>
-            <button onClick={getTopRated}>Top Rated</button>
+                    <input
+                        className='form-control'
+                        name='search'
+                        type='search'
+                        value={value}
+                        onChange={function (event) {
+                            setValue(event.target.value);
+                        }}
+                        onKeyPress={function (event) {
+                            if (event.charCode === 13) {
+                                searching();
+                            }
+                        }}
+                    />
+                </div>
+                <button className='btn btn-info' onClick={searching}>
+                    Search
+                </button>
+                <button className='btn btn-warning' onClick={getTopRated}>
+                    Top Rated
+                </button>
+            </div>
         </header>
     );
 }
--- src/Components/MoviesList/index.js
+++ src/Components/MoviesList/index.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import './index.css';
 function MoviesList() {
     const movies = useSelector(function (state) {
         return state.movies;
     });
     console.log(movies);
     return (
-        <main>
-            {movies.map(function (movie) {
-                return (
-                    <div key={movie.id}>
-                        <h2>
-                            {movie.original_title} ({movie.title})
-                        </h2>
-                        <img src={'https://image.tmdb.org/t/p/w500' + movie.poster_path}></img>
-                        <p>{movie.overview}</p>
-                        <em>
-                            Date: {movie.release_date} - Average: {movie.vote_average}
-                        </em>
-                    </div>
-                );
-            })}
+        <main className='moviesList'>
+            {movies && Array.isArray(movies) && (
+                <>
+                    {movies.length > 0
+                        ? movies.map(function (movie) {
+                              return (
+                                  <div className='card movie' key={movie.id}>
+                                      <img
+                                          className='card-img-top'
+                                          src={'https://image.tmdb.org/t/p/w500' + movie.poster_path}
+                                      ></img>
+                                      <div className='card-body'>
+                                          <h2>
+                                              {movie.original_title} ({movie.title})
+                                          </h2>
+
+                                          <p>{movie.overview}</p>
+                                          <em>
+                                              Date: {movie.release_date} - Average: {movie.vote_average}
+                                          </em>
+                                      </div>
+                                  </div>
+                              );
+                          })
+                        : 'No Movies'}
+                </>
+            )}
         </main>
     );
 }
